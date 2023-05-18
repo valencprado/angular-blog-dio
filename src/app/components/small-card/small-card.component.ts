8a497e409df34de2dda29a71e29c08258b419588
Valen <valenpschool@gmail.com>
2023-05-18 16:37:45 -0300

    utilizando propriedades dinâmicas

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class SmallCardComponent {
 
+  @Input()
+  photoCover:string = '';
+  @Input()
+  cardTitle: string = '';
 }
