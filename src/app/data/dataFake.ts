cae050b0cf5dad0cedfcff3cebaab6724e4b9cc8
Valen <valenpschool@gmail.com>
2023-05-19 12:44:49 -0300

    dados completos

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -1,8 +1,26 @@
 export const dataFake = [
     {
-        "id": 1,
-        "photo":"aaa",
-        "title": "título",
-        "description": "descrição"
-    }
+        "id": "1",
+        "title": "Git e Github: uma colinha necessária!",
+        "description": "Tutoriais simples para quem nunca mexeu com Git e GitHub, duas tecnologias muito importantes para quem anseia trabalhar com TI e desenvolvimento de forma geral. Acesse aqui: https://valencprado.github.io/git-colinha",
+        "photoCover": "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn-images-1.medium.com%2Fmax%2F1200%2F1*qwFrTMnFkcd3U9rFKwwacw.png&f=1&nofb=1&ipt=bd60efd6b2cde7b61e7e72d5b7323c3468be8c2388edad15754dee074b4ad7aa&ipo=images"
+    },
+    {
+        "id": "2",
+        "title": "Filhotinhos fofos",
+        "description": "Filhotes são muito fofos. Se puder, adote um!",
+        "photoCover": "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fsegredosdomundo.r7.com%2Fwp-content%2Fuploads%2F2019%2F07%2Fdescubra-agora-5-cuidados-que-se-deve-ter-com-os-filhotes-de-cachorro-2.jpg&f=1&nofb=1&ipt=8f892d81b015ba3c4271ee98f2390cd9831ae19ded4caafa06588b5e4ac56f0d&ipo=images"
+    },
+    {
+        "id": "3",
+        "title": "Receita de coxinha deliciosa!",
+        "description": "Ingredientes   Massa: 4 xícaras de farinha de trigo 1 litro de água 2 colheres de sopa de vinagre 2 colheres de sopa de caldo de galinha dissolvido em água  Recheio:400 gr de peito de frango cozido e desfiado ½ cebola picada 1 dente de alho picado 4 colheres de sopa de azeite Salsinha picada a gosto Sal a gosto Para empanar: 4 xícaras de farinha de rosca 2 ovos Óleo para fritar Modo de Preparo 1-Em uma panela, coloque um pouco de azeite, doure a cebola e o alho, adicione o frango e a salsinha, mexa bem e reserve.2-Em outra panela, aqueça a água, misture o caldo de galinha o vinagre, 4 colheres de sopa de azeite e espere levantar fervura. Quando atingir esse ponto, despeje 4 xicaras de farinha de trigo e misture bem. Alterne entre o trigo e o azeite até a massa ficar lisa, quando atingir essa textura, deixe cozinhar por 5 minutos.3-Despeje farinha sobre uma superfície plana e coloque a massa de coxinha já fria, sove bem. Abra pequenas porções de massa e recheie com frango, monte a massa dando forma à coxinha.4-Em um prato, bata o ovo e reserve, em outro, coloque a farinha, passe a coxinha primeiro no ovo e depois na farinha de rosca, tome o cuidado para que todos os lados fiquem bem cobertos e empanados.5-Frite em óleo bem quente. Coloque a porção em um prato com papel toalha para secar o excesso de óleo.6-DICA: Se quiser incrementar a receita, a dica é colocar um pouco de catupiry na hora em que for rechear, fica irresistível.",
+        "photoCover": "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fs2.glbimg.com%2FOA5eE_C4dKT8G9PwTc8oVZ9GbaQ%3D%2F0x0%3A630x385%2F984x0%2Fsmart%2Ffilters%3Astrip_icc()%2Fs.glbimg.com%2Fpo%2Frc%2Fmedia%2F2014%2F06%2F04%2F11_00_55_179_receita_tradicional_de_coxinha.jpg&f=1&nofb=1&ipt=3c35d52fb232665ac97ad42a7bd086265f59f1d721236820be419167477f40b5&ipo=images"
+    },
+    {
+        "id": "4",
+        "title": "HTML + CSS + JS: o básico de Front-End",
+        "description": "O HTML é a estrutura da página, o CSS seu estilo e o JavaScript faz a mágica!",
+        "photoCover": "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fphantomlandscapes.files.wordpress.com%2F2021%2F04%2Fhtml-css-javascript.jpg&f=1&nofb=1&ipt=322485e8ab5a3376697528a107b5bf13947d0f81fc290c7aae44e06bdcec6f8c&ipo=images"
+    },
 ]
